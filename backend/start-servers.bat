566e361142f8a954bf285cc235674e4179f3d33c
Abheek Pradhan <163784642+lolout1@users.noreply.github.com>
2024-08-15 22:19:08 -0500

    Update start-servers.bat

--- backend/start-servers.bat
+++ backend/start-servers.bat
@@ -2,12 +2,12 @@
 
 
 
-set URL=%1
+set URL=%1 
 set USER=%2
 
 REM Start ChessCmakeGUI.exe
 start "" "ChessCmakeGUI.exe"
 
 REM Start JSChess.js with Node.js, passing the URL as an argument
-start "" node "JSChess.js" "%URL%" "%USER%" #match-url and optional username if you do not want to build executable (/liChessz/fullCompile/)
+start "" node "JSChess.js" "%URL%" "%USER%" 
 endlocal
